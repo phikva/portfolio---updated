4e4fc81211ebacc13070d587fb837987ebcd3dee
Philip Kvam <ph_kvam@hotmail.com>
2023-03-19 14:08:46 +0100

    milestone conditional grid rendering

--- components/shared/TimelineSection.tsx
+++ components/shared/TimelineSection.tsx
@@ -12,7 +12,7 @@ export function TimelineSection({ timelines }: { timelines: TimelineItem[] }) {
       {timelines?.map((timeline, key) => {
         const { title, milestones } = timeline
         
-        if (milestones.length > 1) {
+        if (milestones.length > 2) {
           return (
          
             <div className="flex flex-col md:grid md:gap-x-5  md:grid-cols-2 xl:grid-cols-3 xl:gap-x-4x " key={key}>
@@ -31,12 +31,32 @@ export function TimelineSection({ timelines }: { timelines: TimelineItem[] }) {
               ))}
             </div>
           )
-        } else 
+        } if (milestones.length === 1) {
+          return (
+         
+            <div className="flex flex-col" key={key}>
+              <h3 className=" mt-20">{title}</h3>
+  
+              
+            
+              {milestones?.map((experience, index) => (
+               
+                <div className='md:pb-20' key={index}>
+                  <TimelineItem
+                    milestone={experience}
+                    isLast={milestones.length - 1 === index}
+                  />
+                </div>
+              ))}
+            </div>
+          )
+         
+        } else
         
         return (
          
-          <div className="flex flex-col" key={key}>
-            <h3 className=" mt-20">{title}</h3>
+          <div className="flex flex-col md:grid md:gap-x-5  md:grid-cols-2 xl:grid-cols-2 xl:gap-x-4x" key={key}>
+          <h3 className=" mt-20 md:col-span-2 xl:col-span-3">{title}</h3>
 
             
 
